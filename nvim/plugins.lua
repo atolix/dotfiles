a9625a23e6ca1f1cac31af33007d4fe049ab4615
atolix <82761106+atolix@users.noreply.github.com>
2023-01-29 22:16:16 +0900

    init

---
+++ nvim/plugins.lua
@@ -0,0 +1,26 @@
+require'packer'.startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use { 'windwp/nvim-autopairs' }
+  use 'j-hui/fidget.nvim'
+  use 'tpope/vim-fugitive'
+  use 'airblade/vim-gitgutter'
+  use 'obaland/vfiler.vim'
+  use 'obaland/vfiler-fzf'
+  use 'ibhagwan/fzf-lua'
+  use 'lambdalisue/fern.vim'
+  use 'lambdalisue/fern-git-status.vim'
+  use 'norcalli/nvim-colorizer.lua'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'dense-analysis/ale'
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  use { 'projekt0n/github-nvim-theme', tag = 'v0.0.7' }
+  use { 'junegunn/fzf', run = ":call fzf#install()" }
+  use { 'junegunn/fzf.vim' }
+  use {'neoclide/coc.nvim', branch = 'release'}
+end)
