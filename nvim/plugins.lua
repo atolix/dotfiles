40d0e743a469659587c295c700757d2c72c94c4a
atolix <siberian.khatru1225@gmail.com>
2023-01-30 18:22:27 +0900

    gitsign config

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -13,8 +13,17 @@ require'packer'.startup(function()
   use {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup()
-  end
+      require('gitsigns').setup({
+         current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+         current_line_blame_opts = {
+           virt_text = true,
+           virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+           delay = 1000,
+           ignore_whitespace = false,
+       },
+         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      })
+    end
   }
   use 'ibhagwan/fzf-lua'
   use 'lambdalisue/fern.vim'
