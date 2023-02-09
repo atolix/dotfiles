9917dc1688d8d9fac9e7a3f8dfc485e80865476b
atolix <82761106+atolix@users.noreply.github.com>
2023-02-09 19:30:44 +0900

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font 'JetBrains Mono',
+  font_size = 12,
+  color_scheme = 'Sequoia Monochrome',
+  window_background_opacity = 0.9
+}
