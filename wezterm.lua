6acf8321f47bc700ec7daf22d80fcbc3cd662d47
atolix <82761106+atolix@users.noreply.github.com>
2023-02-10 00:50:04 +0900

    change wezterm theme

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font 'JetBrains Mono',
   font_size = 12,
-  color_scheme = 'Sequoia Monochrome',
+  -- color_scheme = 'Sequoia Monochrome',
+  color_scheme = 'Sequoia Moonlight',
   window_background_opacity = 0.9
 }
