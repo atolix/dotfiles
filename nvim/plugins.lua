93e53ec87d085a27ae2e0bbacea213d23301d746
atolix <82761106+atolix@users.noreply.github.com>
2023-05-20 00:38:38 +0900

    delete comment

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -13,7 +13,7 @@ require'packer'.startup(function()
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup({
-         current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+         current_line_blame = true,
          current_line_blame_opts = {
            virt_text = true,
            virt_text_pos = 'eol',
