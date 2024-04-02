9f31deaa55a217462a5ebcf4573a237ef8b94c31
atolix <82761106+atolix@users.noreply.github.com>
2024-04-02 22:54:39 +0900

    remove copilot

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -37,6 +37,5 @@ require'packer'.startup(function()
   use { 'junegunn/fzf', run = ":call fzf#install()" }
   use { 'junegunn/fzf.vim' }
   use 'rust-lang/rust.vim'
-  use 'github/copilot.vim'
   use {'neoclide/coc.nvim', branch = 'release'}
 end)
