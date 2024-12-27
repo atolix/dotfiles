713348e45758354a82caab408abfc34ec25ee3d5
atolix <82761106+atolix@users.noreply.github.com>
2024-12-27 09:45:03 +0900

    git clone

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@
 
 # Install Tools
 brew install google-chrome slack docker wezterm starship neovim fzf zoxide eza ripgrep bat volta git tig gh
+
+# clone dotfiles
+git clone git@github.com:atolix/dotfiles.git
