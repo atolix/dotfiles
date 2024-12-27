1c357a49b9735be0e1bc90b00be3509ac6e67259
atolix <82761106+atolix@users.noreply.github.com>
2024-12-27 09:39:52 +0900

    tools

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Tools
+brew install google-chrome slack docker wezterm starship neovim fzf zoxide eza ripgrep bat
