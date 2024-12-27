c3683732907dce007cf80064f070d4b82b175d6e
atolix <82761106+atolix@users.noreply.github.com>
2024-12-27 09:43:23 +0900

    git tools

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Tools
-brew install google-chrome slack docker wezterm starship neovim fzf zoxide eza ripgrep bat volta
+brew install google-chrome slack docker wezterm starship neovim fzf zoxide eza ripgrep bat volta git tig gh
