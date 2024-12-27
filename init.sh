f6eb5b665e8a2a5829f0bcc040fc85340dd203bc
atolix <82761106+atolix@users.noreply.github.com>
2024-12-27 09:48:21 +0900

    git script

--- init.sh
+++ init.sh
@@ -4,5 +4,12 @@
 # Install Tools
 brew install google-chrome slack docker wezterm starship neovim fzf zoxide eza ripgrep bat volta git tig gh
 
-# clone dotfiles
+# Clone dotfiles
 git clone git@github.com:atolix/dotfiles.git
+
+# Get git-completion and git-prompt
+mkdir .zsh
+curl -o ./.zsh/git-completion.zsh  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -o ./.zsh/git-prompt.sh  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+
