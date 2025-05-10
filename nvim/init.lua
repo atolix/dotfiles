b2aa99ffefdac2593ce06d19b5e6d3c6330a2382
atolix <82761106+atolix@users.noreply.github.com>
2025-05-11 01:22:04 +0900

    refacotr luneline

--- nvim/init.lua
+++ nvim/init.lua
@@ -89,11 +89,7 @@ require('lualine').setup {
     lualine_x = {},
     lualine_y = {'progress'},
     lualine_z = {'location'}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+  }
 }
 
 -- nvim-colorizer --
