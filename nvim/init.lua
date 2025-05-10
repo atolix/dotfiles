8316fe4a0151f0506410c77814dcae4224f57180
atolix <82761106+atolix@users.noreply.github.com>
2025-05-11 00:01:34 +0900

    remove winblend

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@ require'plugins'
 vim.opt.fileencoding = "utf-8"
 vim.opt.cursorline = true
 vim.opt.number = true
-vim.opt.winblend = 20
 vim.opt.pumblend = 20
 vim.opt.clipboard:append{'unnamedplus'}
 vim.opt.hidden = true
