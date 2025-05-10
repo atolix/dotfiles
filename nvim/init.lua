9c36bc4fb9be843c1edae187c7fa371c87597792
atolix <82761106+atolix@users.noreply.github.com>
2025-05-11 00:45:30 +0900

    remove sep setting

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,8 +63,6 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
