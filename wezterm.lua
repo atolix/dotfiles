065230009711320d55fc61034c17ed3674e9545e
atolix <82761106+atolix@users.noreply.github.com>
2025-06-26 23:57:05 +0900

    blur

--- wezterm.lua
+++ wezterm.lua
@@ -9,7 +9,7 @@ return {
   adjust_window_size_when_changing_font_size = false,
   default_cursor_style = 'SteadyUnderline',
   window_background_opacity = 0.80,
-  macos_window_background_blur = 10,
+  macos_window_background_blur = 15,
   hide_tab_bar_if_only_one_tab = true,
   show_new_tab_button_in_tab_bar = false,
   window_decorations = 'RESIZE',
