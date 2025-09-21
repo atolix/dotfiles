856c04a72fd3f1f4d7443ba6c5155c38d50eca59
atolix <82761106+atolix@users.noreply.github.com>
2025-09-21 10:42:27 +0900

    del claude

--- nvim/packer.lua
+++ nvim/packer.lua
@@ -40,11 +40,4 @@ require'packer'.startup(function()
 
   -- Utility tools
   use { 'windwp/nvim-autopairs' }
-
-  -- Claude Code Integration
-  use { 'coder/claudecode.nvim',
-    config = function()
-      require('claudecode').setup()
-    end
-  }
 end)
