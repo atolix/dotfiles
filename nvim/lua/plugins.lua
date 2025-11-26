da979ce22bdbabe7c6832117af6be7cafeee1b99
atolix <82761106+atolix@users.noreply.github.com>
2025-11-26 22:20:52 +0900

    remove fugitive

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
   { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-path', 'L3MON4D3/LuaSnip' } },
 
   -- Git integration
-  { 'tpope/vim-fugitive' },
   { 'lewis6991/gitsigns.nvim' },
 
   -- Navigation & File explorer
