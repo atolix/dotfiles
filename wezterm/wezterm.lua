63a873888bae996be4a2f9fde67747d86bf30d32
atolix <ryutaro_mitoda@enpay.co.jp>
2025-11-28 11:07:54 +0900

    remove font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,7 @@ local wezterm = require 'wezterm';
 local act = wezterm.action;
 
 return {
-  font = wezterm.font 'Ricty Diminished',
-  font_size = 14,
+  font_size = 13,
   use_ime = true,
   color_scheme = 'Sequoia Monochrome',
   adjust_window_size_when_changing_font_size = false,
