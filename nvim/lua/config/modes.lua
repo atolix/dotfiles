72e8408c5ac65b83bb205d187d181f64119dff91
atolix <82761106+atolix@users.noreply.github.com>
2025-12-02 22:01:25 +0900

    fix mode color

--- nvim/lua/config/modes.lua
+++ nvim/lua/config/modes.lua
@@ -7,8 +7,8 @@ modes.setup({
   colors = {
     copy = "#f5c359",
     delete = "#c75c6a",
-    insert = "#78ccc5",
-    visual = "#9745be",
+    insert = "#79c0ff",
+    visual = "#a371f7",
   },
   line_opacity = 0.3,
   set_cursor = true,
